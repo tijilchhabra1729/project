002d96090f2464e202eedd24922622f0216923bd
Tijil Chhabra <tijilchhabra@gmail.com>
2023-06-20 14:56:24 +0530

    fixed login issue

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from uuid import uuid4
 
 
@@ -7,7 +8,7 @@ db = SQLAlchemy()
 def get_uuid():
     return uuid4().hex
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
     id = db.Column(db.String(32), primary_key = True, unique=True, default=get_uuid)
     email = db.Column(db.String(345), unique=True)
