9db55d332435de1aab5cac787afd336446edd271
Tijil Chhabra <tijilchhabra@gmail.com>
2023-06-21 15:32:36 +0530

    emission db

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from uuid import uuid4
+from datetime import datetime
 
 
 db = SQLAlchemy()
@@ -14,4 +15,15 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(345), unique=True)
     password = db.Column(db.Text(72), nullable=False)
     name = db.Column(db.String(32))
+    emission = db.relationship('Emission', backref='user', lazy=True)
+
+class Emission(db.Model):
+    __tablename__ = "emissions"
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    datetime = db.Column(db.DateTime, default=datetime.utcnow)
+    week_number = db.Column(db.Integer, default=datetime.utcnow.iscalendar()[1])
+    year = db.Column(db.Integer, default=datetime.utcnow.year)
+    plastic_emission = db.Column(db.Float, default = 0.0)
+    carbon_emission = db.Column(db.Float, default = 0.0)
+    category = db.Column(db.String(32), nullable=False)
 
